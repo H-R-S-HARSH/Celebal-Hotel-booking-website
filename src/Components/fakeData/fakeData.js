8e5c11d8ba965b0de0abe0d0024dd1a483fe5e23
H-R-S-HARSH <116707023+H-R-S-HARSH@users.noreply.github.com>
2023-07-22 18:25:36 +0530

    Add files via upload

---
+++ src/Components/fakeData/fakeData.js
@@ -0,0 +1,67 @@
+// Stay (Hotel) data
+
+export const stay = [
+    {
+        title: 'Light bright airy stylish apt & safe peaceful stay',
+        img: 'Rectangle 26',
+        facilities: [
+            '4 guests 2 bedroom 2 beds 2 baths',
+            'Wifi AC kitchen',
+            'Cancelation flexibility available'
+        ],
+        rating: 4.9,
+        rated_people: 100,
+        price: 59
+    },
+    {
+        title: 'Apartment in Panorama',
+        img: 'Rectangle 27',
+        facilities: [
+            '6 guests 3 bedroom 3 beds 2 baths',
+            'Wifi AC kitchen',
+            'Cancelation flexibility available'
+        ],
+        rating: 4.7,
+        rated_people: 10,
+        price: 89
+    },
+    {
+        title: 'Apartment in Panorama',
+        img: 'Rectangle 28',
+        facilities: [
+            '10 guests 7 bedroom 10 beds 2 baths',
+            'Wifi swimming-pool AC kitchen',
+            'Cancelation flexibility available'
+        ],
+        rating: 5.0,
+        rated_people: 19,
+        price: 199
+    }
+]
+
+
+// From locations
+export const From = ['New Delhi', 'Jaipur', 'Mumbai', 'Patna', 'Lucknow', 'Varanasi', 'Indore', 'Udaipur','Noida','Kolkata','Kanpur'];
+
+
+// Fake Data of Destinatioin
+export const destinations = [
+    {
+        id: 0,
+        name: 'Udaipur',
+        description: "Udaipur is a city in the state of Rajasthan, India, and is also known as the 'City of Lakes'. It is the administrative headquarter of Udaipur district and the historic capital of the kingdom of Mewar in the former Rajputana Agency1. Udaipur is a popular tourist destination known for its azure lakes, magnificent palaces, vibrant culture, and delectable food. It is situated in the hills just east of the Aravalli Range, about 65 miles southwest of Chittaurgarh.",
+        img: 'udaipur'
+    },
+    {
+        id: 1,
+        name: 'Rajgir',
+        description: "Rajgir, meaning 'The City of Kings,' is a historic town in the district of Nalanda in Bihar, India. As the ancient seat and capital of the Haryanka dynasty, the Pradyota dynasty, the Brihadratha dynasty and the Mauryan Empire, as well as the dwelling ground of such historical figures as The Buddha and The Mahavira, the city holds a place of prominence in Hindu, Buddhist and Jain scriptures. As of 2011, the population of the town was reported to be 41,000 while the population in the community development block was about 88,500.",
+        img: 'rajgir'
+    },
+    {
+        id: 2,
+        name: 'Varanasi',
+        description: "Varanasi also Banaras or Benares, and Kashi is a city on the Ganges river in northern India that has a central place in the traditions of pilgrimage, death, and mourning in the Hindu world. The city has a syncretic tradition of Muslim artisanship that underpins its religious tourism.Located in the middle-Ganges valley in the southeastern part of the state of Uttar Pradesh, Varanasi lies on the left bank of the river. It is 692 kilometres (430 mi) to the southeast of India's capital New Delhi and 320 kilometres (200 mi) to the east of the state capital, Lucknow. It lies 121 kilometres (75 mi) downstream of Prayagraj, where the confluence with the Yamuna river is another major Hindu pilgrimage site.",
+        img: 'varanasi'
+    },
+];
